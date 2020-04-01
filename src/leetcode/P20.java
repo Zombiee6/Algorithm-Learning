e713b30b7170825027696329a7ce815d859c15ef
billy <jie5454@gmail.com>
2020-04-01 23:36:50 +0800

    leetcode learning

--- src/leetcode/P20.java
+++ src/leetcode/P20.java
@@ -30,10 +30,7 @@ public class P20 {
                 }
             }
         }
-        if (!stack.isEmpty()) {
-            return false;
-        }
-        return true;
+        return stack.isEmpty();
     }
 
     public static void main(String[] args) {
