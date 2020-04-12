73dbe9ffee3dae563efafadcd393625d64c91618
billy <jie5454@gmail.com>
2020-04-12 16:03:51 +0800

    leetcode learning

--- src/leetcode/P239.java
+++ src/leetcode/P239.java
@@ -2,6 +2,8 @@ package leetcode;
 
 import java.util.*;
 
+import static leetcode.Util.printArray;
+
 /**
  * 239. 滑动窗口最大值
  *
@@ -53,15 +55,16 @@ public class P239 {
 //                return o2.compareTo(o1);
 //            }
 //        });
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(k, (o1, o2) -> o2.compareTo(o1));
+//        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(k, (o1, o2) -> o2.compareTo(o1));
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(k, Comparator.reverseOrder());
         int[] output = new int[n - k + 1];
         for (int i = 0; i < n; i++) {
-            if (i - k + 1 > 0) {
+            if (i + 1 > k) {
                 //remove(object)时间复杂度O(k)，remove()时间复杂度为O(logk)
                 maxHeap.remove(nums[i - k]);
             }
             maxHeap.offer(nums[i]);
-            if (i >= k - 1) {
+            if (i + 1 >= k) {
                 output[i - k + 1] = maxHeap.peek();
             }
         }
@@ -72,22 +75,29 @@ public class P239 {
      * 利用双端队列deque
      * 时间复杂度：O(N)
      * 题目的关键在于队列里面存储的是下标而不是元素值，这样可以解决窗口滑动的问题
+     * 当前窗口下标范围:[i-k+1,i]
+     * 1.移除不在窗口的值（remove，左边出队列）
+     * 2.移除非最大值，使得最大值在队列左边首位（removeLast，右边出队列）
+     * 3.元素入队列(add)
+     * 4.放入结果
      *
      * @param nums
      * @param k
      * @return
      */
     public static int[] maxSlidingWindow3(int[] nums, int k) {
-        int n = nums.length;
-        if (n == 0) {
-            return new int[0];
+        if (nums == null || nums.length == 0 || k < 1 || k > nums.length) {
+            return null;
         }
+        if (k == 1) {
+            return nums;
+        }
+        int n = nums.length;
         int[] output = new int[n - k + 1];
         Deque<Integer> deque = new ArrayDeque<>();
         for (int i = 0; i < n; i++) {
             // 删除队列中小于窗口左边下标的元素
-//            if (i >= k && i - k + 1 > deque.peek()) {
-            if (i + 1 > k && i + 1 - deque.peek() > k) {
+            if (!deque.isEmpty() && i - k + 1 > deque.peek()) {
                 deque.remove();
             }
             // 从队列右侧开始, 删除小于nums[i] 的元素
@@ -96,28 +106,19 @@ public class P239 {
             }
             deque.add(i);
             // 队列左侧是最大值,加入结果
-//            if (i - k + 1 >= 0) {
-            if (i + 1 >= k) {
+            if (i - k + 1 >= 0) {
                 output[i - k + 1] = nums[deque.peek()];
             }
         }
         return output;
     }
 
-    public static void print(int[] array) {
-        for (int i : array) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-    }
-
     public static void main(String[] args) {
 //        int[] nums = {1, 3, -1, -3, 5, 3, 6, 7};
         int[] nums = {1, 3, 1, 2, 0, 5};
-//        int k = 3;
-        int k = 1;
-        print(maxSlidingWindow(nums, k));
-        print(maxSlidingWindow2(nums, k));
-        print(maxSlidingWindow3(nums, k));
+        int k = 3;
+        printArray(maxSlidingWindow(nums, k));
+        printArray(maxSlidingWindow2(nums, k));
+        printArray(maxSlidingWindow3(nums, k));
     }
 }
---
+++ src/leetcode/Util.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+/**
+ * @author Billy
+ * @date 2020/4/12 3:21 下午
+ */
+public class Util {
+    public static void printArray(int[] array) {
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
