e7f129e04c46d60f7e8a30a57a80ca9a938e33e4
billy <jie5454@gmail.com>
2020-09-21 22:50:07 +0800

    leetcode learning

---
+++ src/leetcode/P538.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+/**
+ * 538. 把二叉搜索树转换为累加树
+ *
+ * @author chenjie5
+ * @date 2020/9/21 10:44 下午
+ */
+public class P538 {
+    int sum = 0;
+
+    /**
+     * 反序中序遍历
+     * <p>
+     * 时间复杂度：O(N)
+     * 空间复杂度：O(N)
+     */
+    public TreeNode convertBST(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        convertBST(root.right);
+        sum += root.val;
+        root.val = sum;
+        convertBST(root.left);
+        return root;
+    }
+}
--- src/leetcode/P78.java
+++ src/leetcode/P78.java
@@ -31,6 +31,31 @@ public class P78 {
         return res;
     }
 
+    /**
+     * 回溯法
+     *
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> subsets2(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        backtrack(list, new ArrayList<>(), nums, 0);
+        return list;
+    }
+
+    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums, int start) {
+        //走过的所有路径都是子集的一部分，所以都要加入到集合中
+        list.add(new ArrayList<>(tempList));
+        for (int i = start; i < nums.length; i++) {
+            //做出选择
+            tempList.add(nums[i]);
+            //递归
+            backtrack(list, tempList, nums, i + 1);
+            //撤销选择
+            tempList.remove(tempList.size() - 1);
+        }
+    }
+
     public static void main(String[] args) {
         int[] nums = new int[]{1, 2, 3};
         System.out.println(subsets(nums));
